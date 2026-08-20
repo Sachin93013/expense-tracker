0fa9e81a6e85d741e9393fc8a818f951d950bff3
Sachin <sachinyadav0123321@gmail.com>
2026-08-20 17:11:30 +0530

    Initial project setup

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Expense Tracker API is running"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
