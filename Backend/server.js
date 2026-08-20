13098d633ff4c169f2da000a8e7bb6d888feb1a2
Sachin <sachinyadav0123321@gmail.com>
2026-08-20 18:10:44 +0530

    Create user model and connect MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -19,6 +20,20 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const startServer = async () => {
+    try {
+        await connectDB();
+    
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Server failed:", error.message);
+    }
+};
+
+  startServer();
